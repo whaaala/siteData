b88ad5fce25e823ef7eb249584696f67e18c2aa3
whaaala <wahaaala@gmail.com>
2025-09-03 00:26:04 +0100

    fix: force playwright install at runtime

--- puppeteerPreparation.js
+++ puppeteerPreparation.js
@@ -1,34 +1,35 @@
 import { chromium } from 'playwright'
 
- export default async function preparePuppeteer() {
-  const browser =await chromium.launch({
-     headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ],
+export default async function preparePuppeteer() {
+  const browser = await chromium.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     protocolTimeout: 1000000,
-  });
+  })
   // Create a new page
-  // This will be used to navigate to the URLs and perform actions on the page
-  const page = await browser.newPage();
-
-  // Add the below 1 line of code
-  // This will disable the timeout for navigation, allowing the page to load without timing out
-  // This is useful for pages that take longer to load or have dynamic content
-  page.setDefaultNavigationTimeout(0);
+  // // This will be used to navigate to the URLs and perform actions on the page
+  // const page = await browser.newPage();
 
   // Set a custom user agent to mimic a real browser
   // This helps in avoiding detection as a bot and improves compatibility with some websites
   const customUserAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
 
   // Set custom user agent
   // This will make the requests appear as if they are coming from a real browser
   // This can help in avoiding detection as a bot and improve compatibility with some websites
-  await page.setUserAgent(customUserAgent);
+  const context = await browser.newContext({ userAgent: customUserAgent })
+
+  // Create a new page
+  //This will be used to navigate to the URLs and perform actions on the page
+  const page = await context.newPage()
+
+  // Add the below 1 line of code
+  // This will disable the timeout for navigation, allowing the page to load without timing out
+  // This is useful for pages that take longer to load or have dynamic content
+  page.setDefaultNavigationTimeout(0)
 
   //return the page instance
   // This will be used to navigate to the URLs and perform actions on the page
-  return page;
+  return page
 }
